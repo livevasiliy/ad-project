b6d205c19a74df8556b98c61c3a35c2a3fb49c68
Vasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-08 17:02:46 +0400

    Add UI for form add new Ads

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,19 +1,111 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>New Ad</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Create new ad</h1>
+
+        <v-form ref="form" v-model="valid" lazy-validation class="mb-3">
+          <v-text-field
+            name="title"
+            label="Ad title"
+            type="text"
+            v-model="title"
+            :rules="titleRules"
+            required
+          ></v-text-field>
+          <v-textarea
+            id="description"
+            name="description"
+            label="Description"
+            type="text"
+            v-model="description"
+            :rules="descriptionRules"
+          ></v-textarea>
+        </v-form>
+
+        <v-layout row class="mb-3">
+          <v-flex xs12>
+            <v-btn class="warning">
+              Upload
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <img
+              src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+              alt="ads"
+              height="100"
+            />
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              color="primary"
+              label="Add to promo ?"
+              v-model="promo"
+            ></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row class="mb-3">
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn
+              class="success"
+              @click="createAd"
+              :disabled="!valid"
+            >
+              Create ad
+              <v-icon right dark>done</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'NewAd'
+export default {
+  name: 'NewAd',
+  data () {
+    return {
+      title: '',
+      description: '',
+      promo: false,
+      valid: false,
+      titleRules: [v => !!v || 'Title is required'],
+      descriptionRules: [v => !!v || 'Description is required']
+    }
+  },
+  methods: {
+    onSumbit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+      }
+    },
+    createAd () {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        }
+        console.log(ad)
+      }
+    }
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
