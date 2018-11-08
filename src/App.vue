ff6152d12119d2ad301a3f4251ee4b3e2f6414f6
Vasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-08 13:51:31 +0400

    Add links and finish toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,72 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-tile :to="link.url" v-for="link of links" :key="link.title">
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar app dark color="primary">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon
+        @click="drawer = !drawer;"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title>Ad App</v-toolbar-title>
       <v-spacer></v-spacer>
+
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
+        <v-btn flat v-for="link of links" :key="link.title" :to="link.url">
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <v-content>
-        <router-view></router-view>
-    </v-content>
+
+    <v-content> <router-view></router-view> </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {
+          title: "Login",
+          icon: "lock",
+          url: "/login"
+        },
+        {
+          title: "Registration",
+          icon: "face",
+          url: "/registration"
+        },
+        {
+          title: "Orders",
+          icon: "bookmark_border",
+          url: "/orders"
+        },
+        {
+          title: "New ad",
+          icon: "note_add",
+          url: "/new"
+        },
+        {
+          title: "My ads",
+          icon: "list",
+          url: "/list"
+        }
+      ]
+    };
+  }
+};
 </script>
