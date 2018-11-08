4ad19746d6ce0598b12b9311837e92e8ac9aa2db
Vasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-08 14:54:48 +0400

    Add new components and settings routers

--- src/App.vue
+++ src/App.vue
@@ -36,37 +36,37 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       drawer: false,
       links: [
         {
-          title: "Login",
-          icon: "lock",
-          url: "/login"
+          title: 'Login',
+          icon: 'lock',
+          url: '/login'
         },
         {
-          title: "Registration",
-          icon: "face",
-          url: "/registration"
+          title: 'Registration',
+          icon: 'face',
+          url: '/registration'
         },
         {
-          title: "Orders",
-          icon: "bookmark_border",
-          url: "/orders"
+          title: 'Orders',
+          icon: 'bookmark_border',
+          url: '/orders'
         },
         {
-          title: "New ad",
-          icon: "note_add",
-          url: "/new"
+          title: 'New ad',
+          icon: 'note_add',
+          url: '/new'
         },
         {
-          title: "My ads",
-          icon: "list",
-          url: "/list"
+          title: 'My ads',
+          icon: 'list',
+          url: '/list'
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
