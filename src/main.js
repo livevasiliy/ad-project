f1368c1bc8580770c7b32d6835f4dcc1146677de
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-12 00:16:36 +0400

    Change config data from Firebase

--- src/main.js
+++ src/main.js
@@ -10,15 +10,15 @@ import './stylus/main.styl'
 // import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
-  // theme: {
-  //   primary: "#f44336",
-  //   secondary: "#9575CD",
-  //   accent: "#9c27b0",
-  //   error: "#f44336",
-  //   warning: "#3949AB",
-  //   info: "#2196f3",
-  //   success: "#4caf50"
-  // }
+    // theme: {
+    //   primary: "#f44336",
+    //   secondary: "#9575CD",
+    //   accent: "#9c27b0",
+    //   error: "#f44336",
+    //   warning: "#3949AB",
+    //   info: "#2196f3",
+    //   success: "#4caf50"
+    // }
 })
 Vue.component('app-buy-modal', BuyModalComponent)
 Vue.config.productionTip = false
@@ -28,16 +28,17 @@ new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
+  components: {App},
   template: '<App/>',
+
   created () {
     fb.initializeApp({
-      apiKey: 'AIzaSyD5mgcTCPSiTxPfmTmBmPFWZU7GdR9l1BQ',
-      authDomain: 'itc-ads.firebaseapp.com',
-      databaseURL: 'https://itc-ads.firebaseio.com',
-      projectId: 'itc-ads',
-      storageBucket: 'itc-ads.appspot.com',
-      messagingSenderId: '145551978680'
+      apiKey: 'AIzaSyBZrsPYvGIdIFM2Fb3680RL7Nn5UXvD0QA',
+      authDomain: 'ad-vue.firebaseapp.com',
+      databaseURL: 'https://ad-vue.firebaseio.com',
+      projectId: 'ad-vue',
+      storageBucket: 'ad-vue.appspot.com',
+      messagingSenderId: '527122088432'
     })
 
     fb.auth().onAuthStateChanged(user => {
