77adeb4b69cd92b555b5e94c9f37777adfd4fb0b
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-11-11 18:07:47 +0400

    Add modal window

---
+++ src/components/Ads/EditAdModal.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-dialog width="400px" v-modal="modal">
+      <v-btn class="warning" flat slot="activator">Edit</v-btn>
+      <v-card>
+        <v-container>
+          <v-layout row>
+            <v-flex xs12>
+              <v-card-title>
+                <h1 class="text--primary">Edit ad</h1>
+              </v-card-title>
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout row>
+            <v-flex xs12>
+              <v-card-text>
+                <v-text-field
+                  name="title"
+                  label="Title"
+                  type="text"
+                  v-model="editedTitle"
+                ></v-text-field>
+                <v-text-field
+                  name="description"
+                  label="Description"
+                  type="text"
+                  multi-line
+                  v-model="editedDescription"
+                ></v-text-field>
+              </v-card-text>
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout row>
+            <v-flex xs12>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="error" flat @click="onCancel">Cancel</v-btn>
+                <v-btn class="success" flat @click="onSave">Save</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+      name: 'EditAdModal',
+      props: ['ad'],
+      data () {
+        return {
+          modal: false,
+          editedTitle: this.ad.title,
+          editedDescription: this.ad.description
+        }
+      },
+      methods: {
+        onCancel () {
+          this.editedDescription = this.ad.description
+          this.editedTitle = this.ad.title
+          this.modal = false
+        },
+        onSave () {
+          if (this.editedTitle !== '' && this.editedDescription !== '') {
+            this.$store.dispatch('updateAd', {
+              title: this.editedTitle,
+              description: this.editedDescription,
+              id: this.ad.id
+            })
+            this.modal = false
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
